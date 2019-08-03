932ec6f4b6b8f48cbe678d24527bf67cff9efac9
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-03 13:52:03 +0930

    test: add mock test and eslint to travis

---
+++ test/index.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const should = require('chai').should(); // eslint-disable-line
+
+describe('hexo-renderer-less', () => {
+  // Tests.
+  it('should render less.', () => {
+    return true;
+  });
+});
