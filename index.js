12c86f80f0c5a0498824104be275fa9a71986513
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-07 14:16:06 +0930

    style: eslint

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+/* global hexo */
+'use strict';
+
 const less = require('less');
 const path = require('path');
 
@@ -5,7 +8,7 @@ hexo.extend.renderer.register('less', 'css', (data, options, callback) => {
   const themeConfig = hexo.theme.config.less || {};
   const cwd = process.cwd();
   const paths = (themeConfig.paths || []).map(filepath => {
-    return path.join(cwd, filepath);    // assuming paths are relative from the root of the project
+    return path.join(cwd, filepath); // assuming paths are relative from the root of the project
   });
 
   const parser = less.render(data.text, {
@@ -13,10 +16,10 @@ hexo.extend.renderer.register('less', 'css', (data, options, callback) => {
     filename: path.basename(data.path),
     compress: themeConfig.compress || false
   })
-  .then(output => {
-    callback(null, output.css);
-  },
-  error => {
-    callback(error);
-  });
+    .then(output => {
+      callback(null, output.css);
+    },
+    error => {
+      callback(error);
+    });
 });
