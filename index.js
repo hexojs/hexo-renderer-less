a86f5c6387a6d1ae1f997db5241dc4c87ca14775
curbengh <43627182+curbengh@users.noreply.github.com>
2019-08-08 15:43:36 +0930

    refactor: promise-ify

--- index.js
+++ index.js
@@ -4,22 +4,21 @@
 const less = require('less');
 const path = require('path');
 
-hexo.extend.renderer.register('less', 'css', (data, options, callback) => {
+hexo.extend.renderer.register('less', 'css', (data, options) => {
   const themeConfig = hexo.theme.config.less || {};
   const cwd = process.cwd();
   const paths = (themeConfig.paths || []).map(filepath => {
     return path.join(cwd, filepath); // assuming paths are relative from the root of the project
   });
 
-  const parser = less.render(data.text, { // eslint-disable-line no-unused-vars
-    paths: paths.concat(path.dirname(data.path)),
-    filename: path.basename(data.path),
-    compress: themeConfig.compress || false
-  })
-    .then(output => {
-      callback(null, output.css);
-    },
-    error => {
-      callback(error);
+  return new Promise((resolve, reject) => {
+    less.render(data.text, {
+      paths: paths.concat(path.dirname(data.path)),
+      filename: path.basename(data.path),
+      compress: themeConfig.compress || false
+    }, (err, output) => {
+      if (err) return reject(err);
+      resolve(output.css);
     });
+  });
 });
