53371852577509f17c1da302a52cf11ddb6572cb
curbengh <43627182+curbengh@users.noreply.github.com>
2019-10-02 11:48:24 +0100

    refactor: async-ify

--- index.js
+++ index.js
@@ -4,21 +4,18 @@
 const less = require('less');
 const path = require('path');
 
-hexo.extend.renderer.register('less', 'css', (data, options) => {
+hexo.extend.renderer.register('less', 'css', async (data) => {
   const themeConfig = hexo.theme.config.less || {};
   const cwd = process.cwd();
   const paths = (themeConfig.paths || []).map(filepath => {
     return path.join(cwd, filepath); // assuming paths are relative from the root of the project
   });
 
-  return new Promise((resolve, reject) => {
-    less.render(data.text, {
-      paths: paths.concat(path.dirname(data.path)),
-      filename: path.basename(data.path),
-      compress: themeConfig.compress || false
-    }, (err, output) => {
-      if (err) return reject(err);
-      resolve(output.css);
-    });
+  const result = await less.render(data.text, {
+    paths: paths.concat(path.dirname(data.path)),
+    filename: path.basename(data.path),
+    compress: themeConfig.compress || false
   });
+
+  return result.css;
 });
