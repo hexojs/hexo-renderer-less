550beb17e44c00ba52f6a4ff2e6d840dbb15d0a5
curbengh <43627182+curbengh@users.noreply.github.com>
2019-12-19 04:01:32 +0000

    style: asyncArrow

--- test/index.js
+++ test/index.js
@@ -12,14 +12,14 @@ const r = require('../lib/renderer').bind(ctx);
 describe('less', () => {
   const expected = 'div {\n  bar: 1em;\n}\n';
 
-  it('default', async() => {
+  it('default', async () => {
     const less = { text: '@foo: 1em; div { bar: @foo; }', path: '/foo/bar.less'};
     const result = await r(less);
 
     result.should.eql(expected);
   });
 
-  it('import variable - same folder', async() => {
+  it('import variable - same folder', async () => {
     const filepath = join(process.cwd(), 'test/fixtures/foo.less');
     const less = { text: '@import "variables.less"; div { bar: @foo; }', path: filepath};
     const result = await r(less);
@@ -27,7 +27,7 @@ describe('less', () => {
     result.should.eql(expected);
   });
 
-  it('import variable - different folder', async() => {
+  it('import variable - different folder', async () => {
     ctx.theme.config.less.paths = ['test/fixtures/'];
     const less = { text: '@import "variables.less"; div { bar: @foo; }', path: '/foo/bar.less'};
     const result = await r(less);
